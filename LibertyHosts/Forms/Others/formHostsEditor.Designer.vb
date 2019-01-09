f801b96dd397e3378d5aa042339276ab4224712f
xAsiimov <admin@xasiimov.com>
2019-01-09 10:01:46 +0800

    Code refactoring
    Signed-off-by: xAsiimov <admin@xasiimov.com>

--- LibertyHosts/Forms/Others/formHostsEditor.Designer.vb
+++ LibertyHosts/Forms/Others/formHostsEditor.Designer.vb
@@ -35,7 +35,8 @@ Partial Class formHostsEditor
         'MinimizeButton
         '
         Me.MinimizeButton.Image = Global.LibertyHosts.My.Resources.Resources.MinimizeW
-        Me.MinimizeButton.Location = New System.Drawing.Point(1181, 0)
+        Me.MinimizeButton.Location = New System.Drawing.Point(1772, 0)
+        Me.MinimizeButton.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.MinimizeButton.Name = "MinimizeButton"
         Me.MinimizeButton.Size = New System.Drawing.Size(50, 40)
         Me.MinimizeButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
@@ -45,7 +46,8 @@ Partial Class formHostsEditor
         'CloseButton
         '
         Me.CloseButton.Image = Global.LibertyHosts.My.Resources.Resources.CloseW
-        Me.CloseButton.Location = New System.Drawing.Point(1231, 0)
+        Me.CloseButton.Location = New System.Drawing.Point(1846, 0)
+        Me.CloseButton.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.CloseButton.Name = "CloseButton"
         Me.CloseButton.Size = New System.Drawing.Size(50, 40)
         Me.CloseButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
@@ -55,42 +57,45 @@ Partial Class formHostsEditor
         'Panel
         '
         Me.Panel.BackColor = System.Drawing.Color.White
-        Me.Panel.Font = New System.Drawing.Font("等线", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Panel.Font = New System.Drawing.Font("DengXian", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
         Me.Panel.ForeColor = System.Drawing.Color.Gray
-        Me.Panel.Location = New System.Drawing.Point(0, 57)
+        Me.Panel.Location = New System.Drawing.Point(0, 86)
+        Me.Panel.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Panel.Name = "Panel"
-        Me.Panel.Size = New System.Drawing.Size(53, 666)
+        Me.Panel.Size = New System.Drawing.Size(80, 999)
         Me.Panel.TabIndex = 3
         '
         'RichTextBox
         '
         Me.RichTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.RichTextBox.Font = New System.Drawing.Font("等线", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
-        Me.RichTextBox.Location = New System.Drawing.Point(59, 57)
+        Me.RichTextBox.Font = New System.Drawing.Font("DengXian", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.RichTextBox.Location = New System.Drawing.Point(88, 86)
+        Me.RichTextBox.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.RichTextBox.Name = "RichTextBox"
         Me.RichTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical
-        Me.RichTextBox.Size = New System.Drawing.Size(1222, 666)
+        Me.RichTextBox.Size = New System.Drawing.Size(1831, 997)
         Me.RichTextBox.TabIndex = 2
         Me.RichTextBox.Text = ""
         '
         'SaveButton
         '
         Me.SaveButton.BackColor = System.Drawing.Color.White
-        Me.SaveButton.Font = New System.Drawing.Font("等线", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.SaveButton.Font = New System.Drawing.Font("DengXian", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
         Me.SaveButton.ForeColor = System.Drawing.Color.Black
-        Me.SaveButton.Location = New System.Drawing.Point(59, 11)
+        Me.SaveButton.Location = New System.Drawing.Point(88, 16)
+        Me.SaveButton.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.SaveButton.Name = "SaveButton"
-        Me.SaveButton.Size = New System.Drawing.Size(153, 40)
+        Me.SaveButton.Size = New System.Drawing.Size(230, 60)
         Me.SaveButton.TabIndex = 1
-        Me.SaveButton.Text = "保存文件"
+        Me.SaveButton.Text = "Save Hosts File"
         Me.SaveButton.UseVisualStyleBackColor = False
         '
         'formHostsEditor
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
-        Me.ClientSize = New System.Drawing.Size(1280, 720)
+        Me.ClientSize = New System.Drawing.Size(1920, 1080)
         Me.Controls.Add(Me.SaveButton)
         Me.Controls.Add(Me.RichTextBox)
         Me.Controls.Add(Me.Panel)
@@ -99,6 +104,7 @@ Partial Class formHostsEditor
         Me.ForeColor = System.Drawing.Color.White
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "formHostsEditor"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "formHostsEditor"
--- LibertyHosts/Forms/Others/formMessageBox.Designer.vb
+++ LibertyHosts/Forms/Others/formMessageBox.Designer.vb
@@ -34,7 +34,8 @@ Partial Class formMessageBox
         'CloseButton
         '
         Me.CloseButton.Image = Global.LibertyHosts.My.Resources.Resources.CloseW
-        Me.CloseButton.Location = New System.Drawing.Point(305, 0)
+        Me.CloseButton.Location = New System.Drawing.Point(458, 0)
+        Me.CloseButton.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.CloseButton.Name = "CloseButton"
         Me.CloseButton.Size = New System.Drawing.Size(50, 40)
         Me.CloseButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
@@ -44,55 +45,59 @@ Partial Class formMessageBox
         'TitleLabel
         '
         Me.TitleLabel.AutoSize = True
-        Me.TitleLabel.Font = New System.Drawing.Font("等线", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.TitleLabel.Font = New System.Drawing.Font("DengXian", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
         Me.TitleLabel.ForeColor = System.Drawing.Color.Black
-        Me.TitleLabel.Location = New System.Drawing.Point(6, 8)
+        Me.TitleLabel.Location = New System.Drawing.Point(9, 12)
+        Me.TitleLabel.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.TitleLabel.Name = "TitleLabel"
-        Me.TitleLabel.Size = New System.Drawing.Size(183, 17)
+        Me.TitleLabel.Size = New System.Drawing.Size(276, 25)
         Me.TitleLabel.TabIndex = 3
         Me.TitleLabel.Text = "LibertyHosts MessageBox"
         '
         'OKButton
         '
         Me.OKButton.BackColor = System.Drawing.Color.White
-        Me.OKButton.Font = New System.Drawing.Font("等线", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.OKButton.Font = New System.Drawing.Font("DengXian", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
         Me.OKButton.ForeColor = System.Drawing.Color.Black
-        Me.OKButton.Location = New System.Drawing.Point(35, 99)
+        Me.OKButton.Location = New System.Drawing.Point(52, 148)
+        Me.OKButton.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.OKButton.Name = "OKButton"
-        Me.OKButton.Size = New System.Drawing.Size(138, 33)
+        Me.OKButton.Size = New System.Drawing.Size(207, 50)
         Me.OKButton.TabIndex = 4
-        Me.OKButton.Text = "确定"
+        Me.OKButton.Text = "Confirm"
         Me.OKButton.UseVisualStyleBackColor = False
         '
         'TextLabel
         '
         Me.TextLabel.AutoSize = True
-        Me.TextLabel.Font = New System.Drawing.Font("等线", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.TextLabel.Font = New System.Drawing.Font("DengXian", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
         Me.TextLabel.ForeColor = System.Drawing.Color.Black
-        Me.TextLabel.Location = New System.Drawing.Point(113, 47)
+        Me.TextLabel.Location = New System.Drawing.Point(170, 70)
+        Me.TextLabel.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.TextLabel.Name = "TextLabel"
-        Me.TextLabel.Size = New System.Drawing.Size(126, 17)
+        Me.TextLabel.Size = New System.Drawing.Size(190, 25)
         Me.TextLabel.TabIndex = 5
         Me.TextLabel.Text = "Default TextLabel"
         '
         'NoButton
         '
         Me.NoButton.BackColor = System.Drawing.Color.White
-        Me.NoButton.Font = New System.Drawing.Font("等线", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.NoButton.Font = New System.Drawing.Font("DengXian", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
         Me.NoButton.ForeColor = System.Drawing.Color.Black
-        Me.NoButton.Location = New System.Drawing.Point(179, 99)
+        Me.NoButton.Location = New System.Drawing.Point(268, 148)
+        Me.NoButton.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.NoButton.Name = "NoButton"
-        Me.NoButton.Size = New System.Drawing.Size(138, 33)
+        Me.NoButton.Size = New System.Drawing.Size(207, 50)
         Me.NoButton.TabIndex = 6
-        Me.NoButton.Text = "取消"
+        Me.NoButton.Text = "Cancel"
         Me.NoButton.UseVisualStyleBackColor = False
         '
         'formMessageBox
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
-        Me.ClientSize = New System.Drawing.Size(353, 144)
+        Me.ClientSize = New System.Drawing.Size(530, 216)
         Me.Controls.Add(Me.NoButton)
         Me.Controls.Add(Me.TextLabel)
         Me.Controls.Add(Me.OKButton)
@@ -102,6 +107,7 @@ Partial Class formMessageBox
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.HelpButton = True
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "formMessageBox"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "MessageBox"
